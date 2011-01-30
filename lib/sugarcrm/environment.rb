1e9268e104124cceace7a1b8f0fef91cb0e4faab
davidsulc <david@davidsulc.com>
2011-01-30 11:21:56 -0500

    implement SugarCRM::Environment as a singleton

--- lib/sugarcrm/environment.rb
+++ lib/sugarcrm/environment.rb
@@ -1,7 +1,18 @@
+require 'singleton'
+
 module SugarCRM; class Environment
+  include Singleton
+  
+  def initialize
+  end
+  
   # load all the monkey patch files in the provided folder
-  def self.monkey_patch_folder=(folder, dirstring=nil)
+  def monkey_patch_folder=(folder, dirstring=nil)
     path = File.expand_path(folder, dirstring)
     Dir[File.join(path, '**', '*.rb').to_s].each { |f| load(f) }
+  end
+  
+  def self.method_missing(method_id, *args, &block)
+    self.instance.send(method_id, *args, &block)
   end
 end; end
