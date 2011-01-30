74d5c0cc760db5c5b350350c5def927fa227c0e7
davidsulc <david@davidsulc.com>
2011-01-30 13:05:13 -0500

    refactor path validation

--- lib/sugarcrm/environment.rb
+++ lib/sugarcrm/environment.rb
@@ -8,24 +8,32 @@ module SugarCRM; class Environment
   def initialize
     @config = {}
     
-    default_config_path = File.join(File.dirname(__FILE__), 'config', 'config.yaml')
-    load_config default_config_path if File.exists? default_config_path
+    load_config File.join(File.dirname(__FILE__), 'config', 'config.yaml')
   end
   
   def load_config(path)
+    validate_path path
     config = YAML.load_file(path)
-    config["config"].each{|k,v|
-      @config[k.to_sym] = v
-    }
+    if config && config["config"]
+      config["config"].each{|k,v|
+        @config[k.to_sym] = v
+      }
+    end
   end
   
   # load all the monkey patch files in the provided folder
   def monkey_patch_folder=(folder, dirstring=nil)
+    validate_path folder
     path = File.expand_path(folder, dirstring)
     Dir[File.join(path, '**', '*.rb').to_s].each { |f| load(f) }
   end
   
   def self.method_missing(method_id, *args, &block)
     self.instance.send(method_id, *args, &block)
+  end
+  
+  private
+  def validate_path(path)
+    raise "Invalid path: #{path}" unless File.exists? path
   end
 end; end
