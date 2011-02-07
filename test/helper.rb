8e33542af1d5b255fc475fae13a8ff4d047b0c07
davidsulc <david@davidsulc.com>
2011-02-06 22:27:08 -0500

    raise helpful error if test/config.yaml file is not found

--- test/helper.rb
+++ test/helper.rb
@@ -9,5 +9,7 @@ require 'sugarcrm'
 class Test::Unit::TestCase
   # put your credentials into a YAML file in the test directory
   # the format should be identical to the config_test.yaml found in the same directory
-  SugarCRM::Environment.load_config(File.join(File.dirname(__FILE__),'config.yaml'))
+  path = File.join(File.dirname(__FILE__),'config.yaml')
+  raise "test/config.yaml file not found. See README for instructions on setting up a testing environment" unless File.exists? path
+  SugarCRM::Environment.load_config(path)
 end
