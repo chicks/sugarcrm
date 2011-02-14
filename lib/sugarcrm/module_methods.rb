74df5d3ffed378a0f58b3af77e34361d69838489
davidsulc <david@davidsulc.com>
2011-02-13 20:57:13 -0500

    refactor: store sessions in a hash

--- lib/sugarcrm/module_methods.rb
+++ lib/sugarcrm/module_methods.rb
@@ -1,19 +1,29 @@
 module SugarCRM
-  @@sessions = []
+  # store the various connected sessions
+  # key = session.id, value = session instance
+  @@sessions = {}
   def self.sessions
     @@sessions
+  end
+  
+  def self.add_session(session)
+    @@sessions[session.id] = session
+  end
+  
+  def self.remove_session(session)
+    @@sessions.delete(session.id)
   end
   
   def self.session
     return nil if @@sessions.size < 1
     (raise SugarCRM::MultipleSessions, "There are multiple active sessions") if @@sessions.size > 1
-    @@sessions.first
+    @@sessions.values.first
   end
   
   def self.connection
     return nil if @@sessions.size == 0
     (raise SugarCRM::MultipleSessions, "There are multiple active sessions: use the session instance instead of SugarCRM") if @@sessions.size > 1
-    @@sessions.first.connection
+    @@sessions.values.first.connection
   end
   def self.connect(url, user, pass, options={})
     session = SugarCRM::Session.new(url, user, pass, options)
@@ -33,13 +43,13 @@ module SugarCRM
   def self.current_user
     (raise SugarCRM::NoActiveSession, "No session is active. Create a new session with 'SugarCRM.connect(...)'") if @@sessions.size < 1
     (raise SugarCRM::MultipleSessions, "There are multiple active sessions: call methods on the session instance instead of SugarCRM") if @@sessions.size > 1
-    @@sessions.first.namespace_const.current_user
+    SugarCRM.session.namespace_const.current_user
   end
   
   def self.method_missing(sym, *args, &block)
     (raise SugarCRM::NoActiveSession, "No session is active. Create a new session with 'SugarCRM.connect(...)'") if @@sessions.size < 1
     (raise SugarCRM::MultipleSessions, "There are multiple active sessions: call methods on the session instance instead of SugarCRM") if @@sessions.size > 1
-    @@sessions.first.send(sym, *args, &block)
+    SugarCRM.session.send(sym, *args, &block)
   end
   
   # If a user tries to access a SugarCRM class before they're logged in,
@@ -51,7 +61,7 @@ module SugarCRM
     # if we're logged in, modules should be loaded and available
     if SugarCRM.connection && SugarCRM.connection.logged_in?
       # if user calls (e.g.) SugarCRM::Account, delegate to SugarCRM::Namespace0::Account
-      namespace_const = @@sessions.first.namespace_const
+      namespace_const = SugarCRM.session.namespace_const
       if namespace_const.const_defined? sym
         namespace_const.const_get(sym)
       else
