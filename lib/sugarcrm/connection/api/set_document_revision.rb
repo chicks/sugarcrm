831da231350e2ba28281dfe2fdad2b27acbac8f7
Carl Hicks <carl.hicks@gmail.com>
2011-02-21 10:51:01 -0800

    Update set_document_revision to match API parameters

--- lib/sugarcrm/connection/api/set_document_revision.rb
+++ lib/sugarcrm/connection/api/set_document_revision.rb
@@ -1,12 +1,26 @@
 module SugarCRM; class Connection
   # Sets a new revision for a document.
-  def set_document_revision(id, revision)
+  def set_document_revision(document_id, revision_number, opts={})
+    options = { 
+      :file => '', 
+      :file_name => '' 
+    }.merge! opts
+    
+    # Raise an exception of we try to pass :file, but not :file_name
+    if (!options[:file].empty? && options[:file_name].empty?)
+      raise ArgumentException, ":file_name must be specified if :file is specified"
+    end
+    
     login! unless logged_in?
     json = <<-EOF
       {
         "session": "#{@session.id}",
-        "document_revision": "#{revision}",
-        "id": #{id}
+        "document_revision": {
+           "id": "#{document_id}",
+           "filename": "#{options[:filename]}",
+           "file": "#{Base64.encode64(options[:file])}",
+           "revision": "#{revision_number}"
+        }
       }
     EOF
     json.gsub!(/^\s{6}/,'')
---
+++ test/connection/test_set_document_revision.rb
@@ -0,0 +1,9 @@
+require 'helper'
+
+class TestSetDocumentRevision < ActiveSupport::TestCase
+  context "A SugarCRM.connection" do
+    should "Update a document revision" do
+      # Need to add a test for this!
+    end
+  end
+end
