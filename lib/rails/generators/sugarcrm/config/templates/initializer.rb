387af7f31588758eeba127ba761ed8f30dfa6f57
davidsulc <david@davidsulc.com>
2011-02-26 13:09:12 -0500

    enhance generator to connect to appropriate SugarCRM instance (according to Rails.env)

--- lib/rails/generators/sugarcrm/config/templates/initializer.rb
+++ lib/rails/generators/sugarcrm/config/templates/initializer.rb
@@ -1,1 +1,4 @@
-SugarCRM::Session.new_from_file(File.join(Rails.root, 'config', 'sugarcrm.yml'))
+# Load the values in config/sugarcrm.yml into a hash
+config_values = SugarCRM::Session.parse_config_file(File.join(Rails.root, 'config', 'sugarcrm.yml'))
+# Connect to appropriate SugarCRM instance (depending on Rails environment)
+SugarCRM::Session.from_hash(config_values[Rails.env.to_sym])
